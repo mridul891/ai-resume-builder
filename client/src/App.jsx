ee9349fde106c478f7c2bbf3582f5e415f9b6ac3
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-07-09 08:22:42 +0530

    Completed Header with authentication and Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,25 +4,29 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import SigninPage from "./Pages/SigninPage";
 import { useEffect } from "react";
 import HomePage from "./Pages/HomePage";
+import Header from "./custom/Header";
+import Dashboard from "./Pages/Dashboard";
 
-function App() {
-  const { user, isLoaded, isSignedIn } = useUser();
+const App = () => {
+  const { isLoaded, isSignedIn } = useUser();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isSignedIn && isLoaded) {
       navigate("/auth/sign-in");
     }
-  }, []);
+  }, [isSignedIn, isLoaded]);
 
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth/sign-in" element={<SigninPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
-}
+};
 
 export default App;
---
+++ client/src/custom/Header.jsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { UserButton, useUser } from "@clerk/clerk-react";
+const Header = () => {
+  const { user, isSignedIn } = useUser();
+  return (
+    <div className="p-3 px-5 flex justify-between items-center shadow-md">
+      <Link to={"/"}>
+        <img src={logo} height={100} width={100} />
+      </Link>
+      {isSignedIn ? (
+        <div className="flex gap-2 items-center ">
+          <Link to={"/dashboard"}>
+            <Button>DashBoard</Button>
+          </Link>
+          <UserButton />
+        </div>
+      ) : (
+        <Link to={"/auth/sign-in"}>
+          <Button>Get Started</Button>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default Header;
